1c7b662e553ee60995c7ff7af63056c1434f4acd
TehreemSultan <tehreemsultanali@gmail.com>
2021-03-16 21:05:29 +0500

    detect bar and background

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import './App.css';
 import Navigation from './components/navigation/Navigation'; 
 import Logo from './components/logo/Logo'; 
+import Rank from './components/rank/Rank'; 
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm'; 
+import Particles from 'react-particles-js';
+
+const particleparam={
+  particles: {
+     number:{
+       value:120,
+       density:{
+         enable:true,
+         value_area:1000
+       }
+     }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+     <Particles params={particleparam} className="particles"/>
     <Navigation/>
     <Logo/>
+    <Rank/>
     <ImageLinkForm/>
     </div>
   );
